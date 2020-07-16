36c6f39ce7be38f1a59040a456bbfd67c555e7c0
lucas11200 <lucas11200@hotmail.com>
2020-07-15 23:48:54 -0300

    Adicionado primeiro test, config do RTL

---
+++ src/pages/components/main/test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('Should render Main heading', () => {
+    render(<Main />)
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+  })
+})
